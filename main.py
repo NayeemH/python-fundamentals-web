3044fe925cb4fa91858a031f965a3fb535dbf80c
NayeemH <Nayeemh67@gmail.com>
2024-03-20 23:13:57 +0600

    add database integrations

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookings.db'
+db = SQLAlchemy(app)
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), nullable = False)
+    service = db.Column(db.String(50), nullable = False)
 
 @app.route('/')
 def index():
@@ -14,9 +23,20 @@ def booking():
 def book():
     name = request.form.get('name')
     service = request.form.get('service')
-    return f"Thank you, {name}, for booking {service}!"
+
+    booking = Booking (name=name, service=service)
+    db.session.add(booking)
+    db.session.commit()
+    return redirect(url_for('bookings'))
+
+@app.route('/bookings') # to show all bookings details
+def bookings():
+    all_bookings = Booking.query.all()
+    return render_template('bookings.html', bookings=all_bookings)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all() # create database tables before running the app
     app.run(debug=True)
 
 
