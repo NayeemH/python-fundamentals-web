09d5aadfa034d1ac52c07e6e741de743e6d27dfd
NayeemH <Nayeemh67@gmail.com>
2024-03-23 23:20:43 +0600

    add authentications

--- main.py
+++ main.py
@@ -1,10 +1,58 @@
 from flask import Flask, redirect, render_template, request, url_for
+from flask_login import (LoginManager, UserMixin, current_user, login_required,
+                         login_user, logout_user)
+from flask_principal import Permission, Principal, RoleNeed
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookings.db'
+app.config['SECRET_KEY'] = 'secret_key'
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+principal = Principal(app)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(50), unique = True, nullable = False ) 
+    password = db.Column(db.String(50), nullable = False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            return 'Invalid username or password! Please check your credentials'
+    return render_template('login.html')
+
+@app.route('/logout')
+
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return 'Username already exists! Try with a different username'
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
 
 class Booking(db.Model):
     id = db.Column(db.Integer, primary_key = True)
